ab58953b16ebd0ffaf8361e7aea213fe10e63d4f
F220318 <f220318@F220318mac.local>
2024-09-28 18:59:15 +0900

    DB接続の追加

---
+++ backend/db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+func InitDB() (db *gorm.DB, err error) {
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(db)/%s?charset=utf8mb4&parseTime=true&loc=Local",
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_DATABASE"),
+	)
+
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // use singular table name, table for `User` would be `user` with this option enabled
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connected!")
+	return db, nil
+}
--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"trip-planner/db"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
 
+	err := godotenv.Load()
+	if err != nil {
+		e.Logger.Fatal("error loading .env file")
+	}
+
+	db, err := db.InitDB()
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	fmt.Println(db)
 	// テスト
-    e.GET("/api/test", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello, World!")
-    })
-	
+	e.GET("/api/test", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
 	e.Logger.Fatal(e.Start(":8000"))
 }
