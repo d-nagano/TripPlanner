0c250ad7a5325a6e72c23fa150ec5c283dda061e
F220318 <f220318@F220318mac.local>
2024-10-20 16:29:20 +0900

    vuerouter/typescrip の追加

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="about">
+    <ul>
+      <li v-for="prefecture in prefectures" :key="prefecture.id">
+        {{ prefecture.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { fetchPrefectures } from '../hooks/prefectures';
+import type { Prefecture } from '../types/types';
+
+export default defineComponent({
+  setup() {
+      const prefectures = ref<Prefecture[]>([]);
+      const error = ref<string | null>(null);
+
+      const getPrefectures = async () => {
+          try {
+              prefectures.value = await fetchPrefectures();
+          } catch (err) {
+              error.value = 'データの取得に失敗しました。';
+          }
+      };
+
+      getPrefectures();
+    
+      return {
+          prefectures,
+          error,
+      };
+  },
+});
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
