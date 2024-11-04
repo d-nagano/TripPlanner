f240203fb17eb27723b837f888913b5e5f48f20e
F220318 <f220318@F220318mac.local>
2024-11-04 23:22:21 +0900

    zerolog の追加

--- backend/handlers/handler.go
+++ backend/handlers/handler.go
@@ -2,14 +2,18 @@ package handlers
 
 import (
 	"trip-planner/infra"
+
+	"github.com/rs/zerolog"
 )
 
 type AppHandler struct {
 	AppConfig *infra.AppConfig
+	Logger    zerolog.Logger
 }
 
-func NewHandler(appConfig *infra.AppConfig) *AppHandler {
+func NewHandler(appConfig *infra.AppConfig, logger zerolog.Logger) *AppHandler {
 	return &AppHandler{
 		AppConfig: appConfig,
+		Logger:    logger,
 	}
 }
