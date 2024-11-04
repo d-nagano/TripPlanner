6b79d56adc50754deed907556c572a011a1ffcb7
F220318 <f220318@F220318mac.local>
2024-11-04 23:11:03 +0900

    レビュー後修正

--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -2,13 +2,12 @@
     <div>
       <form @submit.prevent="onSubmit">
         <input v-model="user.name" placeholder="名前" required />
-        <!-- type="email" -->
-        <input v-model="user.email" placeholder="メールアドレス" required />
+        <input v-model="user.email" type="email" placeholder="メールアドレス" required />
         <input v-model="user.password" type="password" placeholder="パスワード" required />
         <button type="submit">サインアップ</button>
       </form>
   
-      <p v-if="error">{{ error }}</p>
+      <p v-if="error" class="error">{{ error }}</p>
     </div>
 </template>
   
@@ -17,6 +16,7 @@
   import { defineComponent, ref } from 'vue';
   import { fetchUserSignUp } from '../hooks/user';
   import type { User } from '../types/types';
+  import axios from 'axios';
   
   export default defineComponent({
     setup() {
@@ -30,9 +30,12 @@
         const signup = async (user: User) => {
             try {
                 await fetchUserSignUp(user);
-            } catch (err: any) {
-                console.log(err)
-                alert(err.response.data)
+            } catch (err) {
+                if (axios.isAxiosError(err) && err.response) {
+                    error.value = err.response.data;
+                } else {
+                    error.value = '予期せぬエラーが発生しました';
+                }
             }
         };
   
