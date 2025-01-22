e7cd3e12c41399e787aecee41a78faceb9c8ce4c
F220318 <f220318@F220318mac.local>
2025-01-23 01:08:38 +0900

    not null制約の追加

--- backend/models/trip_plan.go
+++ backend/models/trip_plan.go
@@ -8,11 +8,11 @@ import (
 
 type TripPlan struct {
 	ID            string    `gorm:"type:varchar(36);primaryKey" json:"id"`
-	Title         string    `gorm:"type:varchar(64)" json:"title"`
-	Destination   string    `gorm:"type:varchar(64)" json:"destination"`
+	Title         string    `gorm:"type:varchar(64);not null" json:"title"`
+	Destination   string    `gorm:"type:varchar(64);not null" json:"destination"`
 	DepartureDate time.Time `gorm:"type:datetime" json:"departure_date"`
 	ArrivalDate   time.Time `gorm:"type:datetime" json:"arrival_date"`
-	UserID        string    `json:"user_id"`
+	UserID        string    `gorm:"type:varchar(36);not null" json:"user_id"`
 	User          User      `gorm:"foreignKey:UserID"`
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
