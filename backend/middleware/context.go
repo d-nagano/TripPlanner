b92f03a8c75e581e43ee286dd7cd950662f83626
F220318 <f220318@F220318mac.local>
2025-02-13 17:41:32 +0900

    JWTトークンの取得の際にパニックを防ぐ

--- backend/middleware/context.go
+++ backend/middleware/context.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"log/slog"
 	"net/http"
 	"trip-planner/common"
 	"trip-planner/infra"
@@ -8,15 +9,18 @@ import (
 
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
+	slogecho "github.com/samber/slog-echo"
 	"gorm.io/gorm"
 )
 
 func ContextMiddleware(db *gorm.DB) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			user := c.Get("user").(*jwt.Token)
-			claims := user.Claims.(*infra.JwtCustomClaims)
-			userID := claims.UserID
+			userID := validateToken(c)
+			if  userID == ""{
+				slogecho.AddCustomAttributes(c, slog.String("error msg", "invalid token"))
+				return c.JSON(http.StatusUnauthorized, "トークンが不正です。")
+			}
 
 			uRepo := repos.NewUserRepo(db)
 			actingUser, err := uRepo.FindByID(userID)
@@ -32,4 +36,23 @@ func ContextMiddleware(db *gorm.DB) echo.MiddlewareFunc {
 			return next(ctx)
 		}
 	}
+}
+
+func validateToken(c echo.Context) string {
+	userToken := c.Get("user")
+	if userToken == nil {
+		return ""
+	}
+
+	user, ok := userToken.(*jwt.Token)
+	if !ok {
+		return ""
+	}
+
+	claims, ok := user.Claims.(*infra.JwtCustomClaims)
+	if !ok {
+		return ""
+	}
+
+	return claims.UserID
 }
